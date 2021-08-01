a03f57c5ccdb375347b8af2324204c578a0f06a2
hassanimn <mohsen2hasani@gmail.com>
2021-08-01 15:47:11 +0430

    Test project created

---
+++ MH.Excel.Test/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using MH.Excel.Export;
+using MH.Excel.Test.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MH.Excel.Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var list = new TestClass().GetList();
+
+            var excel = await ExportManager.ExportToXlsxAsync<TestClass, TestClass.SubClassTest>(list, "Test Excel Class");
+
+            return File(excel.FileContents, excel.ContentType, excel.FileDownloadName);
+        }
+    }
+}
---
+++ MH.Excel.Test/Models/TestClass.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MH.Excel.Test.Models
+{
+    public class TestClass
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string Name2 { get; set; }
+
+
+        [Display(Name = "Items")]
+        public List<SubClassTest> List { get; set; } = new();
+
+
+        public List<TestClass> GetList()
+        {
+            var tests = new List<TestClass>();
+
+            for (int i = 1; i < 10; i++)
+            {
+                var test = new TestClass
+                {
+                    Id = i,
+                    Name = $"Name - {i}",
+                    Name2 = $"Name2 - {i}",
+                };
+
+                for (int j = 1; j < 10; j++)
+                {
+                    test.List.Add(new SubClassTest
+                    {
+                        Id = 50 * j,
+                        Type = $"Type - {j}",
+                        Type2 = $"Type2 - {j}",
+                        Type3 = $"Type3 - {j}"
+                    });
+                }
+
+                tests.Add(test);
+            }
+
+            return tests;
+        }
+
+        public class SubClassTest
+        {
+            [Display(Name = "Id")]
+            public int Id { get; set; }
+
+            [Display(Name = "attr1")]
+            public string Type { get; set; }
+
+            [Display(Name = "attr2")]
+            public string Type2 { get; set; }
+
+            [Display(Name = "attr3")]
+            public string Type3 { get; set; }
+        }
+    }
+}
