b7027a3c66136e55b7a7b9457ee50c4a7a11f7fa
Mohsen Hasani <m.hasani@alibaba.ir>
2023-12-04 17:12:18 +0330

    use minimal Program.cs class

--- MH.Excel.Test/Controllers/HomeController.cs
+++ MH.Excel.Test/Controllers/HomeController.cs
@@ -1,43 +1,41 @@
-﻿using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using MH.Excel.Export;
 using MH.Excel.Test.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace MH.Excel.Test.Controllers
+namespace MH.Excel.Test.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
 
-        public async Task<IActionResult> Excel1()
-        {
-            var list = new TestClass().GetList();
+    public async Task<IActionResult> Excel1()
+    {
+        var list = new TestClass().GetList();
 
-            var excel = await ExportManager.ExportToXlsxAsync(list, "Test Simple Excel");
+        var excel = await ExportManager.ExportToXlsxAsync(list, "Test Simple Excel");
 
-            return File(excel.FileContents, excel.ContentType, excel.FileDownloadName);
-        }
+        return File(excel.FileContents, excel.ContentType, excel.FileDownloadName);
+    }
 
-        public async Task<IActionResult> Excel2()
-        {
-            var list = new TestClass().GetList();
+    public async Task<IActionResult> Excel2()
+    {
+        var list = new TestClass().GetList();
 
-            var excel = await ExportManager.ExportToXlsxAsync<TestClass, SubClassTest>(list, "Test 2 Level Excel");
+        var excel = await ExportManager.ExportToXlsxAsync<TestClass, SubClassTest>(list, "Test 2 Level Excel");
 
-            return File(excel.FileContents, excel.ContentType, excel.FileDownloadName);
-        }
+        return File(excel.FileContents, excel.ContentType, excel.FileDownloadName);
+    }
 
-        public async Task<IActionResult> Excel3()
-        {
-            var list = new TestClass().GetList();
+    public async Task<IActionResult> Excel3()
+    {
+        var list = new TestClass().GetList();
 
-            var excel = await ExportManager.ExportToXlsxAsync<TestClass, SubClassTest, SubClassTest2>(list, "Test 2 Level Excel");
+        var excel = await ExportManager.ExportToXlsxAsync<TestClass, SubClassTest, SubClassTest2>(list, "Test 2 Level Excel");
 
-            return File(excel.FileContents, excel.ContentType, excel.FileDownloadName);
-        }
+        return File(excel.FileContents, excel.ContentType, excel.FileDownloadName);
     }
 }
--- MH.Excel.Test/Models/TestClass.cs
+++ MH.Excel.Test/Models/TestClass.cs
@@ -1,98 +1,97 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace MH.Excel.Test.Models
+namespace MH.Excel.Test.Models;
+
+public class TestClass
 {
-    public class TestClass
+    public List<TestClass> GetList()
     {
-        public List<TestClass> GetList()
+        var tests = new List<TestClass>();
+
+        for (var i = 1; i < 10; i++)
         {
-            var tests = new List<TestClass>();
-
-            for (var i = 1; i < 10; i++)
+            var test = new TestClass
             {
-                var test = new TestClass
-                {
-                    Id = i,
-                    Name = $"Name - {i}",
-                    LastName = $"LastName - {i}",
-                    Description = "Lorem ipsum is placeholder text"
-                };
+                Id = i,
+                Name = $"Name - {i}",
+                LastName = $"LastName - {i}",
+                Description = "Lorem ipsum is placeholder text"
+            };
 
-                for (var j = 1; j < 10; j++)
+            for (var j = 1; j < 10; j++)
+            {
+                test.List.Add(new SubClassTest
                 {
-                    test.List.Add(new SubClassTest
-                    {
-                        Id = 50 * j,
-                        Type = $"Type - {j}",
-                        Type2 = $"Type2 - {j}",
-                        Type3 = $"Lorem ipsum is placeholder text - {j}"
-                    });
-                }
-
-                for (var j = 1; j < 10; j++)
-                {
-                    test.List2.Add(new SubClassTest2
-                    {
-                        Id = 50 * j,
-                        Type = $"Type - {j}",
-                        Type2 = $"Type2 - {j}",
-                        Type3 = $"Lorem ipsum is placeholder text - {j}"
-                    });
-                }
-
-                tests.Add(test);
+                    Id = 50 * j,
+                    Type = $"Type - {j}",
+                    Type2 = $"Type2 - {j}",
+                    Type3 = $"Lorem ipsum is placeholder text - {j}"
+                });
             }
 
-            return tests;
+            for (var j = 1; j < 10; j++)
+            {
+                test.List2.Add(new SubClassTest2
+                {
+                    Id = 50 * j,
+                    Type = $"Type - {j}",
+                    Type2 = $"Type2 - {j}",
+                    Type3 = $"Lorem ipsum is placeholder text - {j}"
+                });
+            }
+
+            tests.Add(test);
         }
 
-        [Display(Name = "Id")]
-        public int Id { get; set; }
-
-        [Display(Name = "First Name")]
-        public string Name { get; set; }
-
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-
-        [Display(Name = "About user")]
-        public string Description { get; set; }
-
-        [Display(Name = "Items")]
-        public List<SubClassTest> List { get; set; } = new();
-
-        [Display(Name = "Items 2")]
-        public List<SubClassTest2> List2 { get; set; } = new();
+        return tests;
     }
 
-    public class SubClassTest
-    {
-        [Display(Name = "Id")]
-        public int Id { get; set; }
+    [Display(Name = "Id")]
+    public int Id { get; set; }
 
-        [Display(Name = "attr1")]
-        public string Type { get; set; }
+    [Display(Name = "First Name")]
+    public string Name { get; set; }
 
-        [Display(Name = "attr2")]
-        public string Type2 { get; set; }
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
 
-        [Display(Name = "attr3")]
-        public string Type3 { get; set; }
-    }
+    [Display(Name = "About user")]
+    public string Description { get; set; }
 
-    public class SubClassTest2
-    {
-        [Display(Name = "Id 2")]
-        public int Id { get; set; }
+    [Display(Name = "Items")]
+    public List<SubClassTest> List { get; set; } = new();
 
-        [Display(Name = "attr1 2")]
-        public string Type { get; set; }
+    [Display(Name = "Items 2")]
+    public List<SubClassTest2> List2 { get; set; } = new();
+}
 
-        [Display(Name = "attr2 2")]
-        public string Type2 { get; set; }
+public class SubClassTest
+{
+    [Display(Name = "Id")]
+    public int Id { get; set; }
 
-        [Display(Name = "attr3 2")]
-        public string Type3 { get; set; }
-    }
+    [Display(Name = "attr1")]
+    public string Type { get; set; }
+
+    [Display(Name = "attr2")]
+    public string Type2 { get; set; }
+
+    [Display(Name = "attr3")]
+    public string Type3 { get; set; }
+}
+
+public class SubClassTest2
+{
+    [Display(Name = "Id 2")]
+    public int Id { get; set; }
+
+    [Display(Name = "attr1 2")]
+    public string Type { get; set; }
+
+    [Display(Name = "attr2 2")]
+    public string Type2 { get; set; }
+
+    [Display(Name = "attr3 2")]
+    public string Type3 { get; set; }
 }
